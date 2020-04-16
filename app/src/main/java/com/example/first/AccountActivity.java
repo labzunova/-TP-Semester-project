e9b943c1419992d5ac161acd70e3117db74d9780
labzunova <61713797+labzunova@users.noreply.github.com>
2020-04-16 14:06:36 +0300

    commit for collaborative work

--- app/src/main/java/com/example/first/AccountActivity.java
+++ app/src/main/java/com/example/first/AccountActivity.java
@@ -1,14 +1,43 @@
 package com.example.first;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private Button mExitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        mExitButton = (Button) findViewById(R.id.exitBtn);
+
+        mExitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(AccountActivity.this,AuthorizationActivity.class));
+            }
+        });
+
     }
+
+
 }
